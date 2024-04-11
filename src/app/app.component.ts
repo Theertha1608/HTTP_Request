95a429795e8720c3c28da7d7e85589f5585a86f8
theetha1608 <theerthakunjusss@gmail.com>
2024-04-11 21:59:42 +0530

    Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpService } from './http.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'http-req';
+  userDetails: any;
+
+  constructor(private httpService: HttpService) { }
+
+  getUserDetails() {
+    this.httpService.getUserDetails().subscribe((data: any) => {
+      this.userDetails = data;
+    });
+  }
 }
